e696b2e4ad1645b81a768f99e931d68e9eb560a9
harsha <121601031+harshabavigadda@users.noreply.github.com>
2023-08-25 11:14:15 +0530

    Update bubble_sort.js

--- Script/bubble_sort.js
+++ Script/bubble_sort.js
@@ -1,18 +1,12 @@
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    
-    *****************
-*/
 
 function Bubble()
 {
-    //Setting Time complexities
+    
     document.getElementById("Time_Worst").innerText="O(N^2)";
     document.getElementById("Time_Average").innerText="Θ(N^2)";
     document.getElementById("Time_Best").innerText="Ω(N)";
 
-    //Setting Space complexity
+    
     document.getElementById("Space_Worst").innerText="O(1)";
 
     c_delay=0;
@@ -43,10 +37,3 @@ function Bubble()
 
     enable_buttons();
 }
-
-/*
-    *****************
-    DONE BY:-   TUMMALA KETHAN
-    
-    *****************
-*/
